6c5724feab175943668c579b28b860accdeffa0f
James Batsell <jamesbatsell05@gmail.com>
2019-09-11 22:22:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma if 
   greeting == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+  elsif greeting == greeting.upcase
+  return "HUH?! SPEAK UP SONNY!"
+else
+  
   
   
   
