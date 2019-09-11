3f2179f2d3d9a493979820386dc887ae2d271dd1
James Batsell <jamesbatsell05@gmail.com>
2019-09-11 22:14:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma if
   phrase == "hi nana, how are you?" 
   puts "HUH?! SPEAK up, SONNY!"
   elsif
-  
+  greeting == greeting.upcase
   puts "NO, NOT SINCE 1938!"
 else
   phrase == "I LOVE YOU GRANDMA!"
