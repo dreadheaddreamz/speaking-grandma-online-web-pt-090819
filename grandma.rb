509e61dd44064be13421cf84e1134e448fee0952
James Batsell <jamesbatsell05@gmail.com>
2019-09-11 22:19:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,4 @@
 def speak_to_grandma if 
   greeting == "Hi nana, how are you?" 
   puts "HUH?! SPEAK up, SONNY!"
-  elsif
-  greeting.upcase == "What did you eat today?"
-  puts "NO, NOT SINCE 1938!"
-else
-  greeting == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-end
-end
+  
